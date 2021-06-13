ea3c4f801f5e0c21206e69c37cce17c42ec55c39
Wonho Zhung <59193965+WonhoZhung@users.noreply.github.com>
2021-06-13 23:24:17 +0900

    Update basis.py

--- Hartree-Fock_H2/basis.py
+++ Hartree-Fock_H2/basis.py
@@ -2,10 +2,23 @@ import numpy as np
 
 class Gaussian():
   
-  def __init__(self, a, d):
-    """
-    params
-    a:: 
-    """
-    self.a = a
-    self.d = d
+    def __init__(self, a, d):
+        """
+        params
+        a:: Gaussian Exponents
+        d:: Contraction Coefficients
+        """
+        self.a = a
+        self.d = d
+        
+    def prod(self, g1, g2):
+        """
+        inputs
+        g1, g2:: Gaussian class
+        
+        output
+        g:: Gaussian class, product of g1 & g2
+        """
+        return g
+    
+  
