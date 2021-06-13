f7acfd35d84559faba1e99d2b276c2fd40e80e7a
Wonho Zhung <59193965+WonhoZhung@users.noreply.github.com>
2021-06-13 23:33:46 +0900

    Update basis.py

--- Hartree-Fock_H2/basis.py
+++ Hartree-Fock_H2/basis.py
@@ -2,14 +2,14 @@ import numpy as np
 
 class Gaussian():
   
-    def __init__(self, a, d):
+    def __init__(self, a, R):
         """
         params
         a:: Gaussian Exponents
-        d:: Contraction Coefficients
+        d:: Gaussian Centers
         """
         self.a = a
-        self.d = d
+        self.R = R
         
     def prod(self, g1, g2):
         """
@@ -19,6 +19,26 @@ class Gaussian():
         output
         g:: Gaussian class, product of g1 & g2
         """
+        p = g1.a + g2.a
+        R_p = (g1.a*g1.R+g2.a*g2.R)/p
+        g = Gaussian(p, R_p)
         return g
-    
+      
+        
+class STO_3G():
   
+    def __init__(self, as, Rs, ds):
+        """
+        params
+        as:: list of a
+        Rs:: list of R
+        ds:: list of d, contraction coefficients
+        """
+        assert len(as) == len(Rs) == len(ds)
+        self.as = as
+        self.Rs = Rs
+        self.ds = ds
+        
+        self.gs = [(Gaussian(a, R), d) for a, R, d in zip(as, Rs, ds)]
+        self.N = len(self.gs)
+      
