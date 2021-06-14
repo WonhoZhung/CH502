0e79ddda8c0e3bee7627fb633aa236a61f3d798d
WonhoZhung <zpeter97@kaist.ac.kr>
2021-06-14 19:38:35 +0900

    update plot.py

--- Hartree-Fock_H2/plot.py
+++ Hartree-Fock_H2/plot.py
@@ -20,14 +20,15 @@ for d in tqdm([0.01*x for x in range(50,450)]):
     R_list = mol.coords
     basis_set = [STO_3G(A_LIST, D_LIST, R) for R in R_list]
 
-    _, _, e, E = run(basis_set, mol)
+    _, _, _, E = run(basis_set, mol)
     nuc_rep = nuc_repulsion(mol)
 
     x_list.append(d)
     y_list.append(E+nuc_rep)
 
 plt.plot(x_list, y_list)
-plt.xlabel("Distance (Å)")
+#plt.xlabel("Distance (Å)")
+plt.xlabel("Distance (Bohr)")
 plt.ylabel("Dissociation Energy (Hartree)")
 plt.title("Dissociation_Curve_H2")
 plt.savefig("Dissociation_Curve_H2.png")
